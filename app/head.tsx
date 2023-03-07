d43c5567cd2a89f20e728dbc09c27118c4b02098
Benito Pedro <benitopedro21@outlook.com>
2023-03-07 14:15:05 -0300

    changing title

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Head = () => (
 	<React.Fragment>
-		<title>Metaversus</title>
+		<title>Frontend Madness</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="icon" href="/favicon.ico" />
 	</React.Fragment>
