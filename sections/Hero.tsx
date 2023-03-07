1ab012fd315161eb00b3f7a84417c8509c919a87
Benito Pedro <benitopedro21@outlook.com>
2023-03-07 14:21:46 -0300

    changing img tags to Image tags

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 
 import { motion } from "framer-motion";
 import styles from "../styles";
@@ -36,18 +37,22 @@ const Hero = () => {
 				>
 					<div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
 
-					<img
+					<Image
 						src="/cover.png"
 						alt="cover"
 						className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+						width="350"
+						height="350"
 					/>
 
 					<a href="#explore">
 						<div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
-							<img
+							<Image
 								src="/stamp.png"
 								alt="stamp"
 								className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
+								width="100"
+								height="100"
 							/>
 						</div>
 					</a>
