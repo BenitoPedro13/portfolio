5f85a737c14b96ee1646cf96a59bfb71541ba908
benito pedro <benitopedro21@outlook.com>
2023-03-09 17:01:24 -0300

    WIP: fix eslint

--- components/CustomTexts.tsx
+++ components/CustomTexts.tsx
@@ -1,30 +1,29 @@
 'use client';
 
-import { motion } from "framer-motion";
-import React from "react";
-import styles from "../styles";
-import { fadeIn, staggerContainer, textContainer, textVariant2 } from "../utils/motion.js";
+import { motion } from 'framer-motion';
+import React from 'react';
+import { textContainer, textVariant2 } from '../utils/motion.js';
 
-export const TypingText = ({textStyles, title}: any) => (
+export const TypingText = ({ textStyles, title }) => (
   <motion.p
-  variants={textContainer}
-  className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
->
-  {Array.from(title).map((letter, index) => (
-    <motion.span variants={textVariant2} key={index}>
-      {letter === ' ' ? '\u00A0' : letter as React.ReactNode}
-    </motion.span>
-  ))}
-</motion.p>
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+  >
+    {Array.from(title).map((letter, index) => (
+      <motion.span variants={textVariant2} key={index}>
+        {letter === ' ' ? '\u00A0' : letter}
+      </motion.span>
+    ))}
+  </motion.p>
 );
 
-export const TitleText = ({textStyles, title}: any) => (
+export const TitleText = ({ textStyles, title }) => (
   <motion.h2
-  variants={textVariant2}
-  initial="hidden"
-  whileInView="show"
-  className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
->
-  {title}
-</motion.h2>
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+  >
+    {title}
+  </motion.h2>
 );
--- components/ExploreCard.tsx
+++ components/ExploreCard.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 import styles from '../styles';
 import { fadeIn } from '../utils/motion';
 
-const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }: any) => (
+const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div
     variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
     className={`relative ${
