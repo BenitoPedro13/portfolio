d4f22492d870761552a0f25f4415ad2771f91d0b
benito pedro <benitopedro21@outlook.com>
2023-03-16 09:36:05 -0300

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,19 @@
 import "../styles/globals.css";
-
-const RootLayout = ({ children }: any) => (
-	<html lang="en">
-		<head>
-			<link rel="preconnect" href="https://stijndv.com" />
-			<link
-				rel="stylesheet"
-				href="https://stijndv.com/fonts/Eudoxus-Sans.css"
-			/>
-		</head>
-		<body>{children}</body>
-	</html>
+import { Analytics } from "@vercel/analytics/react";
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+  <html lang="en">
+    <head>
+      <link rel="preconnect" href="https://stijndv.com" />
+      <link
+        rel="stylesheet"
+        href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+      />
+    </head>
+    <body>
+      {children}
+      <Analytics />
+    </body>
+  </html>
 );
 
 export default RootLayout;
