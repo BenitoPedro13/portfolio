ae992312d06c90ce6ce6276e5185cf1c74ab6638
benito pedro <benitopedro21@outlook.com>
2023-03-16 16:15:58 -0300

    Fix Hero

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -19,13 +19,14 @@ const Hero = () => {
 						variants={textVariant(1.1)}
 						className={`${styles.heroHeading}`}
 					>
-						Desenvolvendo 
+						Frontend
 					</motion.h1>
 					<motion.div
 						variants={textVariant(1.2)}
 						className="flex flex-row justify-center items-center"
 					>
-						<h1 className={styles.heroHeading}>Universos</h1>
+						<div className={`${styles.heroDText}`} />
+						<h1 className={styles.heroHeading}>eveloper</h1>
 					</motion.div>
 				</div>
 				<motion.div
@@ -35,7 +36,7 @@ const Hero = () => {
 					<div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
 
 					<img
-						src="/cover.webp"
+						src="/cover.png"
 						alt="cover"
 						className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
 					/>
@@ -43,7 +44,7 @@ const Hero = () => {
 					<a href="#explore">
 						<div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
 							<img
-								src="/stamp.webp"
+								src="/stamp.png"
 								alt="stamp"
 								className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
 							/>
