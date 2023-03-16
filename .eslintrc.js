22efdf670fc44060210a529232595df14c3c4c85
benito pedro <benitopedro21@outlook.com>
2023-03-16 11:29:59 -0300

    FiX

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    "next",
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,16 @@
 import "../styles/globals.css";
-import { Analytics } from "@vercel/analytics/react";
-const RootLayout = ({ children }: { children: React.ReactNode }) => (
-  <html lang="en">
-    <head>
-      <link rel="preconnect" href="https://stijndv.com" />
-      <link
-        rel="stylesheet"
-        href="https://stijndv.com/fonts/Eudoxus-Sans.css"
-      />
-    </head>
-    <body>
-      {children}
-      <Analytics />
-    </body>
-  </html>
+
+const RootLayout = ({ children }: any) => (
+	<html lang="en">
+		<head>
+			<link rel="preconnect" href="https://stijndv.com" />
+			<link
+				rel="stylesheet"
+				href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+			/>
+		</head>
+		<body>{children}</body>
+	</html>
 );
 
 export default RootLayout;
