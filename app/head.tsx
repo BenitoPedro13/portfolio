650a89f75727245d687e919b3b6f4119a5e5d18d
benito pedro <benitopedro21@outlook.com>
2023-03-16 09:17:32 -0300

    change title of html

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Head = () => (
 	<React.Fragment>
-		<title>Frontend Madness</title>
+		<title>Benito Tech</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="icon" href="/favicon.ico" />
 	</React.Fragment>
