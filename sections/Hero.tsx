df0e101fe4a9e8fddf68b5562a1f66a4b52444fc
benito pedro <benitopedro21@outlook.com>
2023-03-19 00:48:57 -0300

    cover

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -36,7 +36,7 @@ const Hero = () => {
 				>
 					<div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
 
-					<Image
+					<img
 						src="/cover.webp"
 						alt="cover"
 						className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
